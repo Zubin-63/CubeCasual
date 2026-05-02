02bc900df3a369759d403a14a79b55a896a627c2
Zubin-63 <zubinjamwal@gmail.com>
2026-05-02 22:51:59 +0530

    Added Player and player  movement with bounds

---
+++ Assets/Scripts/PlayerScripts/BoundsScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minX = -2.6f, maxX = 2.6f, minY = -5.6f, maxY = 5.6f;
+    private bool isOutOfBounds;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckBounds();
+    }
+    public void CheckBounds()
+    {
+        Vector2 temp=transform.position;
+        if (temp.x > maxX)
+        {
+            temp.x=maxX;
+        }
+        if(temp.x < minX)
+        {
+            temp.x=minX;
+        }
+        transform.position = temp;
+        if (temp.y <= minY)
+        {
+            if (!isOutOfBounds)
+            {
+                isOutOfBounds = true;
+                // sound of death
+                // gamemanager restart game 
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/MovementScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D myBody;
+    public float moveSpeed=2f;
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+
+    }
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        
+    }
+    private void Move()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myBody.velocity = new Vector2(moveSpeed,myBody.velocity.y);
+        }
+        if(Input.GetAxisRaw("Horizontal")<0f)
+        {
+            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        }
+    }
+    public void PlatformMove(float x)
+    {
+        myBody.velocity = new Vector2(x, myBody.velocity.y);
+    }
+}
